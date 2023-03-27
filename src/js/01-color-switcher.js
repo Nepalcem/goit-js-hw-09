4b4387ede05a6efa4386fc3bf1c5578fb7faca74
Nepalcem <mark.emailtest@gmail.com>
2023-03-28 01:12:26 +0300

    task -1 color-switcher

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,28 @@
+const start = document.querySelector('button[data-start]');
+const stop = document.querySelector('button[data-stop]');
+let timerID = null;
 
+start.addEventListener('click', timedColorChange);
+stop.addEventListener('click', timedColorStop);
+
+function onClickBodyColorChange() {
+  const color = getRandomHexColor();
+  document.body.style.backgroundColor = color;
+  start.setAttribute('disabled', true);
+}
+
+function timedColorStop() {
+  clearInterval(timerID);
+  start.removeAttribute('disabled');
+}
+
+function timedColorChange() {
+  onClickBodyColorChange();
+  timerID = setInterval(onClickBodyColorChange, 1000);
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215)
+    .toString(16)
+    .padStart(6, 0)}`;
+}
