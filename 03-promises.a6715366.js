1099583a3cb183641333d75c0dec050d81afe4b0
Nepalcem <57187696+Nepalcem@users.noreply.github.com>
2023-03-28 22:06:38 +0000

    Deploying to gh-pages from @ Nepalcem/goit-js-hw-09@f0ca693c3767213fc96d1bdfe36707bb81a5df06 🚀

---
+++ 03-promises.a6715366.js
@@ -0,0 +1,2 @@
+const e=document.querySelector(".form"),{delay:l,step:t,amount:u}=e.elements;l.value=0,t.value=0,u.value=0;
+//# sourceMappingURL=03-promises.a6715366.js.map
---
+++ 03-promises.f81956f8.js
@@ -0,0 +1,2 @@
+!function(){var e=document.querySelector(".form").elements,u=e.delay,a=e.step,l=e.amount;u.value=0,a.value=0,l.value=0}();
+//# sourceMappingURL=03-promises.f81956f8.js.map
