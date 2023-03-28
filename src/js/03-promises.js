f0ca693c3767213fc96d1bdfe36707bb81a5df06
Nepalcem <mark.emailtest@gmail.com>
2023-03-29 01:06:11 +0300

    task-3 init

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,3 +1,10 @@
+const form = document.querySelector('.form');
+// console.log(form.elements);
+const { delay, step, amount } = form.elements;
+delay.value = 0;
+step.value = 0;
+amount.value = 0;
+
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
   if (shouldResolve) {
