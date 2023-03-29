a7b02d5c1cda7a99c92b7aad17f7c07d7dc931a5
Nepalcem <mark.emailtest@gmail.com>
2023-03-29 16:54:35 +0300

    task-3 finish

--- src/js/03-promises.js
+++ src/js/03-promises.js
@@ -1,15 +1,34 @@
+import Notiflix from 'notiflix';
+
 const form = document.querySelector('.form');
-// console.log(form.elements);
-const { delay, step, amount } = form.elements;
-delay.value = 0;
-step.value = 0;
-amount.value = 0;
+form.addEventListener('submit', formHandler);
+
+function formHandler(e) {
+  const {
+    elements: { delay, step, amount },
+  } = e.currentTarget;
+  e.preventDefault();
+  const firstDelay = Number(delay.value);
+  const promiseStep = Number(step.value);
+  const promisesAmount = Number(amount.value);
+  let delayvalue = firstDelay;
+  for (i = 1; i <= promisesAmount; i += 1) {
+    createPromise(i, delayvalue)
+      .then(value => Notiflix.Notify.success(value, { timeout: 10000 }))
+      .catch(error => Notiflix.Notify.failure(error, { timeout: 10000 }));
+    delayvalue += promiseStep;
+  }
+}
 
 function createPromise(position, delay) {
   const shouldResolve = Math.random() > 0.3;
-  if (shouldResolve) {
-    // Fulfill
-  } else {
-    // Reject
-  }
+  return new Promise((resolve, reject) => {
+    setTimeout(() => {
+      if (shouldResolve) {
+        resolve(`✅ Fulfilled promise ${position} in ${delay}ms`);
+      } else {
+        reject(`❌ Rejected promise ${position} in ${delay}ms`);
+      }
+    }, delay);
+  });
 }
